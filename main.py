1f6aa7c804693725f9770bb59bc68241cddc239a
Nguyễn Mậu Trường Giang <giangnmt98@gmail.com>
2025-01-08 14:54:08 +0700

    Add feathr_jar path handling and git tag creation script
    Introduced handling for `feathr_jar_path` in `main.py` to ensure the JAR file is copied to the current directory when defined. Added a `create_git_tag.sh` script to simplify creating, deleting, and recreating Git tags. Also removed the unused `vnpt_feathr-0.0.1.jar` file.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import os
 import argparse
+import shutil
 
 from featurestore.base.utils.utils import load_simple_dict_config
 from featurestore.feature_pipeline import FeaturePipeline
@@ -16,7 +18,22 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
     config = load_simple_dict_config(args.config_path)
-    # main(args.config_path)
+    try:
+        feathr_jar_path = config.get("feathr_jar_path")
+        if feathr_jar_path:
+            current_directory = os.getcwd()
+            destination_file = os.path.join(current_directory, os.path.basename(feathr_jar_path))
+
+            # Kiểm tra nếu file chưa tồn tại thì thực hiện copy
+            if not os.path.exists(destination_file):
+                shutil.copy(feathr_jar_path, destination_file)
+                print(f"File copied from {feathr_jar_path} to {destination_file}")
+            else:
+                print(f"File already exists at {destination_file}, skipping copy.")
+        else:
+            print("Config 'feathr_jar_path' is not defined in the configuration.")
+    except Exception as e:
+        print(f"Error copying file: {e}")
 
     FeaturePipeline(
         raw_data_path=config["raw_data_path"],
